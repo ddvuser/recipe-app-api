2b5c70293fb69fe06e902a0b70e20182f64ebedb
Denys Doroshenko <denisdoroshenko99@gmail.com>
2024-03-12 02:04:55 +0200

    add env file

--- app/app/settings.py
+++ app/app/settings.py
@@ -1,9 +1,9 @@
 from pathlib import Path
+import os
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = "django-insecure-!l490at^hpig%1q1=(s)!l8&upxdqvg2dgmw5v^u@cu210#053"
-
+SECRET_KEY = os.environ.get("SECRET_KEY")
 DEBUG = True
 
 ALLOWED_HOSTS = []
@@ -49,8 +49,11 @@ WSGI_APPLICATION = "app.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "HOST": os.environ.get("DB_HOST"),
+        "NAME": os.environ.get("DB_NAME"),
+        "USER": os.environ.get("DB_USER"),
+        "PASSWORD": os.environ.get("DB_PASS"),
     }
 }
 
