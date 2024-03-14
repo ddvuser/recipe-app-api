a983caa8c231b1ddd0361a2ccce9227d12d0a537
Denys Doroshenko <denisdoroshenko99@gmail.com>
2024-03-14 16:08:40 +0200

    add UserAdmin class, add admin site tests

---
+++ app/core/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+
+from core import models
+
+
+class UserAdmin(BaseUserAdmin):
+    """Admin page for users."""
+
+    ordering = ["id"]
+    list_display = ["email", "name"]
+
+
+admin.site.register(models.User, UserAdmin)
