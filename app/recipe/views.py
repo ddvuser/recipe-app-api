80a86d21b0f535bca3aa54834e78d749168e584b
Denys Doroshenko <denisdoroshenko99@gmail.com>
2024-03-20 22:46:11 +0200

    add tests for filtering tags and ingredients

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -2,6 +2,12 @@
 Views for the recipe APIs.
 """
 
+from drf_spectacular.utils import (
+    extend_schema_view,
+    extend_schema,
+    OpenApiParameter,
+    OpenApiTypes,
+)
 from rest_framework import (
     viewsets,
     mixins,
@@ -16,6 +22,22 @@ from core.models import Recipe, Tag, Ingredient
 from recipe import serializers
 
 
+@extend_schema_view(
+    list=extend_schema(
+        parameters=[
+            OpenApiParameter(
+                "tags",
+                OpenApiTypes.STR,
+                description="Comma separated list of IDs to filter",
+            ),
+            OpenApiParameter(
+                "ingredients",
+                OpenApiTypes.STR,
+                description="Comma separated list of ingredient IDs to filter",
+            ),
+        ]
+    )
+)
 class RecipeViewSet(viewsets.ModelViewSet):
     """View for managing recipe APIs."""
 
@@ -24,9 +46,23 @@ class RecipeViewSet(viewsets.ModelViewSet):
     authentication_classes = [TokenAuthentication]
     permission_classes = [IsAuthenticated]
 
+    def _params_to_ints(self, qs):
+        """Convert a list of strings to integers."""
+        return [int(str_id) for str_id in qs.split(",")]
+
     def get_queryset(self):
-        """Retrieve recipes fro authenticatedd user."""
-        return self.queryset.filter(user=self.request.user).order_by("-id")
+        """Retrieve recipes for authenticatedd user."""
+        tags = self.request.query_params.get("tags")
+        ingredients = self.request.query_params.get("ingredients")
+        qs = self.queryset
+        if tags:
+            tag_ids = self._params_to_ints(tags)
+            qs = qs.filter(tags__id__in=tag_ids)
+        if ingredients:
+            ingredients_ids = self._params_to_ints(ingredients)
+            qs = qs.filter(ingredients__id__in=ingredients_ids)
+
+        return qs.filter(user=self.request.user).order_by("-id").distinct()
 
     def get_serializer_class(self):
         """Return the serializer class for request."""
