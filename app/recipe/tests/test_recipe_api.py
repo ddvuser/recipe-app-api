78b994b83fcb304873b31a117335713c0f9ac58f
Denys Doroshenko <denisdoroshenko99@gmail.com>
2024-03-20 21:13:00 +0200

    add tests for filtering recipes

--- app/recipe/tests/test_recipe_api.py
+++ app/recipe/tests/test_recipe_api.py
@@ -404,6 +404,53 @@ class PrivateRecipeAPITests(TestCase):
         self.assertEqual(res.status_code, status.HTTP_200_OK)
         self.assertEqual(recipe.ingredients.count(), 0)
 
+    def test_filter_by_tags(self):
+        """Test filtering recipes by tags."""
+        r1 = create_recipe(user=self.user, title="RecipeFoo")
+        r2 = create_recipe(user=self.user, title="RecipeBar")
+
+        tag1 = Tag.objects.create(user=self.user, name="TagFoo")
+        tag2 = Tag.objects.create(user=self.user, name="TagBar")
+
+        r1.tags.add(tag1)
+        r2.tags.add(tag2)
+
+        r3 = create_recipe(user=self.user, title="RecipeBuzz")
+
+        params = {"tags": f"{tag1.id}, {tag2.id}"}
+        res = self.client.get(RECIPES_URL, params)
+
+        s1 = RecipeSerializer(r1)
+        s2 = RecipeSerializer(r2)
+        s3 = RecipeSerializer(r3)
+        self.assertIn(s1.data, res.data)
+        self.assertIn(s2.data, res.data)
+        self.assertNotIn(s3.data, res.data)
+
+    def test_filter_by_ingredients(self):
+        """Test filtering recipes by ingredients."""
+        r1 = create_recipe(user=self.user, title="RecipeFoo")
+        r2 = create_recipe(user=self.user, title="RecipeBar")
+
+        ing1 = Ingredient.objects.create(user=self.user, name="IngFoo")
+        ing2 = Ingredient.objects.create(user=self.user, name="IngBar")
+
+        r1.ingredients.add(ing1)
+        r2.ingredients.add(ing2)
+
+        r3 = create_recipe(user=self.user, title="RecipeBuzz")
+
+        params = {"ingredients": f"{ing1.id}, {ing2.id}"}
+        res = self.client.get(RECIPES_URL, params)
+
+        s1 = RecipeSerializer(r1)
+        s2 = RecipeSerializer(r2)
+        s3 = RecipeSerializer(r3)
+
+        self.assertIn(s1.data, res.data)
+        self.assertIn(s2.data, res.data)
+        self.assertNotIn(s3.data, res.data)
+
 
 class ImageUploadTests(TestCase):
     """Tests for the image upload API."""
